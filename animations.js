dd81b5c430e9351ca8c1fed5fd0fac2ac10e0c25
Joshua Bendig <cyberpunk@knuddels.de>
2021-08-31 18:06:00 +0200

    more sprites, changed script

--- animations.js
+++ animations.js
@@ -26,8 +26,45 @@ export const ANIMATIONS = {
             h: 141,
             dest_x: -2,
             dest_y: 20
+        },  
+    ],
+    'edgeworth_talk': [
+        {
+            sheet: 'edgeworth.png',
+            x: 22,
+            y: 250,
+            w: 107,
+            h: 144,
+            dest_x: 100,
+            dest_y: 20
         },
-        
+        {
+            sheet: 'edgeworth.png',
+            x: 147,
+            y: 250,
+            w: 107,
+            h: 144,
+            dest_x: 100,
+            dest_y: 20
+        },
+        {
+            sheet: 'edgeworth.png',
+            x: 271,
+            y: 250,
+            w: 107,
+            h: 144,
+            dest_x: 100,
+            dest_y: 20
+        }, 
+        {
+            sheet: 'edgeworth.png',
+            x: 405,
+            y: 250,
+            w: 107,
+            h: 144,
+            dest_x: 100,
+            dest_y: 20
+        },
     ]
 }
 
@@ -41,5 +78,15 @@ export const BACKGROUNDS = {
         fsh: 46,
         x: 0,
         y: 147
-    }
+    },
+    'prosecutor': {
+        background: "prosecutorempty.png",
+        foreground: "ui.png",
+        fsx: 616,
+        fsy: 898,
+        fsw: 206,
+        fsh: 46,
+        x: 50,
+        y: 147
+    },
 }
